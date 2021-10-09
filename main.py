bcfdd0d644b478d6e714b3bc3eff77dbaf2591c9
Phil Elementz <tfeabp@gmail.com>
2021-10-09 11:48:24 +0200

    New function that helps download everything with special symbols in the title

--- main.py
+++ main.py
@@ -8,30 +8,39 @@ bad_extension = ".mp4"
 good_extension = ".mp3"
 
 
-def checking_folder():
+def check_folder():
     """Creates a folder where files will be downloaded"""
     path = input("Enter the name of the folder to store the playlist: ")
     global folder
     # Change the folder path to your wish
     folder = f"M:\Muza\mp3\YoutubePlaylists\{path}"
-    check_folder = os.path.isdir(folder)
-    if not check_folder:
+    checking_folder = os.path.isdir(folder)
+    if not checking_folder:
         os.makedirs(folder)
-        print(f"The {folder} folder has been created.")
+        print(f"The {path} folder has been created.")
     else:
-        print(f"The {folder} folder already exists.")
+        print(f"The {path} folder already exists.")
+
+
+def get_rid_of_symbols(path):
+    """Gets rid of the symbols in the name of downloading video that StreamQuery deletes"""
+    return ''.join(c for c in path if (c != '.') and (c != '|') and (c != ':')
+                   and (c != '"') and (c != '\'') and (c != '\\') and (c != '/')
+                   and (c != ',') and (c != '?') and (c != ';') and (c != '/<')
+                   and (c != '>') and (c != '$') and (c != '#'))
 
 
 def run():
-    checking_folder()
+    """Downloads each file in the playlist as a .mp4 and changes it to a .mp3 extension."""
+    check_folder()
     playlist = input("Paste a link to your playlist: ")
     p = Playlist(playlist)
     print(f"Downloading: {p.title}")
     for file in p.videos:
         StreamQuery.filter(file.streams, type="audio", adaptive=True).first().download(folder)
         print(file.title)
-        bad_file = file.title + bad_extension
-        good_file = file.title + good_extension
+        bad_file = get_rid_of_symbols(file.title) + bad_extension
+        good_file = get_rid_of_symbols(file.title) + good_extension
         os.chdir(folder)
         os.rename(bad_file, good_file)
 
