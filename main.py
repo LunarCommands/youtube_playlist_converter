40f388bc799cb0772c4202ef033ce9c0d2a36bfa
LunarCommands <tfeabp@gmail.com>
2022-09-08 14:17:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+from pytube import YouTube
 from pytube import Playlist
-from pytube import StreamQuery
 import os
+import moviepy.editor as mp
+import re
 
 
 folder = ""
-bad_extension = ".mp4"
-good_extension = ".mp3"
 
 
 def check_folder():
-    """Creates a folder where files will be downloaded"""
+    """Creates a folder where files will be downloaded."""
     path = input("Enter the name of the folder to store the playlist: ")
     global folder
     # Change the folder path to your wish
-    folder = f"M:\Muza\mp3\YoutubePlaylists\{path}"
+    folder = f"/Users/lunar_commands/Music/{path}"
     checking_folder = os.path.isdir(folder)
     if not checking_folder:
         os.makedirs(folder)
@@ -22,12 +22,15 @@ def check_folder():
         print(f"The {path} folder already exists.")
 
 
-def get_rid_of_symbols(path):
-    """Gets rid of the symbols in the name of downloading video that StreamQuery deletes"""
-    return ''.join(c for c in path if (c != '.') and (c != '|') and (c != ':')
-                   and (c != '"') and (c != '\'') and (c != '\\') and (c != '/')
-                   and (c != ',') and (c != '?') and (c != ';') and (c != '/<')
-                   and (c != '>') and (c != '$') and (c != '#'))
+def rename():
+    """Changes all .mp4 files to .mp3."""
+    for file in os.listdir(folder):
+        if re.search('mp4', file):
+            mp4_path = os.path.join(folder, file)
+            mp3_path = os.path.join(folder, os.path.splitext(file)[0] + '.mp3')
+            new_file = mp.AudioFileClip(mp4_path)
+            new_file.write_audiofile(mp3_path)
+            os.remove(mp4_path)
 
 
 def run():
@@ -35,19 +38,12 @@ def run():
     check_folder()
     playlist = input("Paste a link to your playlist: ")
     p = Playlist(playlist)
-    print(f"Downloading: {p.title}")
 
-    for file in p.videos:
-        try:
-            StreamQuery.filter(file.streams, type="audio", adaptive=True).first().download(folder)
-            print(file.title)
-            bad_file = get_rid_of_symbols(file.title) + bad_extension
-            good_file = get_rid_of_symbols(file.title) + good_extension
-            os.chdir(folder)
-            os.rename(bad_file, good_file)
-        except FileExistsError:
-            print("File already exists.\n")
-            os.remove(folder + "\\" + get_rid_of_symbols(file.title) + bad_extension)
+    for url in p:
+        YouTube(url).streams.filter(adaptive=True, only_audio=True).first().download(folder)
+        print(f"Downloading: {url}")
+
+    rename()
 
 
 if __name__ == "__main__":
